2bd9f90de7fb11dc16941302fd2a8a57c3aa8baf
EIT-Admin <nzambouarmel@gmail.com>
2024-07-06 11:36:10 +0200

    application debit

--- account/models.py
+++ account/models.py
@@ -52,6 +52,3 @@ class CustomUser(AbstractUser):
         return self.email
 
 
-class Debit(models.Model):
-    nom_debit = models.CharField(max_length=128, blank=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tot')
---
+++ debit/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from account.models import CustomUser
+# Create your models here.
+
+class Debit(models.Model):
+    nom_debit = models.CharField(max_length=128, blank=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='debit')
